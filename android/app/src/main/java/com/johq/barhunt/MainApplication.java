5d1ffcc2bf95ef08d30a08f8ea6d86f12d85f500
Johanna Reinikainen <johanna.reinikainen@hiq.fi>
2019-02-14 13:24:59 +0200

    Facebook login setup started

--- android/app/src/main/java/com/johq/barhunt/MainApplication.java
+++ android/app/src/main/java/com/johq/barhunt/MainApplication.java
@@ -13,11 +13,20 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -27,9 +36,8 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(new MainReactPackage(),
-            new RNFirebasePackage(),
-            new RCTMGLPackage(), new VectorIconsPackage(), new RNLocalizePackage(),
+      return Arrays.<ReactPackage>asList(new MainReactPackage(), new FBSDKPackage(mCallbackManager),
+          new RNFirebasePackage(), new RCTMGLPackage(), new VectorIconsPackage(), new RNLocalizePackage(),
           new RNGestureHandlerPackage(), new RNFirebaseAuthPackage());
     }
 
