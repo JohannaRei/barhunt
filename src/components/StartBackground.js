c9bde2c08854e64ed689171c71c296a16b4f432e
Johanna Reinikainen <johanna.reinikainen@hiq.fi>
2019-04-11 14:51:38 +0300

    Moved svg generation logic to startscreen

--- src/components/StartBackground.js
+++ src/components/StartBackground.js
@@ -1,37 +1,13 @@
 // @flow
 import React from 'react';
 import { Svg, Polygon } from 'react-native-svg';
-import { Dimensions } from 'react-native';
 
-const StartBackground = () => {
-  const { height, width } = Dimensions.get('window');
-  const heights = [height * 0.2, height * 0.4, height * 0.6, height * 0.8];
-  const widths = [width * 0.2, width * 0.4, width * 0.6, width * 0.8, width];
-
-  const generatePoints = (type) => {
-    let points = '0,0';
-    for (let i = -1; i < 6; i += 1) {
-      let w = 0;
-      let h = heights[type];
-      if (i >= 0 && i < 5) {
-        w = widths[i];
-      } else if (i > 4) {
-        w = widths[4];
-        h = 0;
-      }
-      const random = (i + 2) % 2 === 0 ? -30 : 30; // todo: actually make this randomized
-      points += ` ${w},${h + random}`;
-    }
-    return points;
-  };
-
-  return (
-    <Svg height={heights[3].toString()} width={width.toString()}>
-      <Polygon points={generatePoints(2)} fill="yellow" />
-      <Polygon points={generatePoints(1)} fill="orange" />
-      <Polygon points={generatePoints(0)} fill="black" />
-    </Svg>
-  );
-};
+const StartBackground = ({ points, totalHeight, width }) => (
+  <Svg height={totalHeight.toString()} width={width.toString()}>
+    <Polygon points={points[2]} fill="yellow" />
+    <Polygon points={points[1]} fill="orange" />
+    <Polygon points={points[0]} fill="black" />
+  </Svg>
+);
 
 export default StartBackground;
--- src/screens/Onboarding/StartScreen.js
+++ src/screens/Onboarding/StartScreen.js
@@ -1,20 +1,61 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
+import { Dimensions } from 'react-native';
 import { Screen, Text, StartBackground } from '@comp';
 import { Button } from 'native-base';
 
 type Props = {
   navigation: any
 };
+type State = {};
 
-const StartScreen = ({ navigation }: Props) => (
-  <Screen>
-    <StartBackground />
-    <Text tx="startScreen.welcome" />
-    <Button block onPress={() => navigation.navigate('name')}>
-      <Text tx="startScreen.button" />
-    </Button>
-  </Screen>
-);
+class StartScreen extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      widths: [],
+      heights: [],
+      width: 0,
+      points: []
+    };
+  }
+
+  componentWillMount() {
+    const { height, width } = Dimensions.get('window');
+    const heights = [height * 0.15, height * 0.35, height * 0.55, height * 0.75];
+    const widths = [width * 0.2, width * 0.4, width * 0.6, width * 0.8, width];
+    this.setState({ heights, widths, width }, () => this.generatePoints());
+  }
+
+  generatePoints = () => {
+    const { widths, heights, width } = this.state;
+    const allPoints = [];
+    for (let i = 0; i < 3; i += 1) {
+      const points = ['0,0', `0,${heights[i]}`];
+      const nh = widths.map((h, idx) => {
+        const random = idx % 2 === 0 ? Math.random() * 10 + 50 : Math.random() * -50 - 10;
+        return heights[i] + Math.floor(random);
+      });
+      widths.forEach((w, idx) => points.push(`${w},${nh[idx]}`));
+      points.push(`${width},0`);
+      allPoints.push(points.join(' '));
+    }
+    this.setState({ points: allPoints });
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const { points, heights, width } = this.state;
+    return (
+      <Screen>
+        <StartBackground points={points} totalHeight={heights[3]} width={width} />
+        <Text tx="startScreen.welcome" />
+        <Button block onPress={() => navigation.navigate('name')}>
+          <Text tx="startScreen.button" />
+        </Button>
+      </Screen>
+    );
+  }
+}
 
 export default StartScreen;
